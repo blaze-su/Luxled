381583c37ab3d496fa710f4e6c443b747c147b5e
isTerra-maksYgso <maks.kolendyuk@gmail.com>
2017-08-06 16:10:51 +0300

    Вёрстка

--- source/js/app.js
+++ source/js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     catalogCheckBox();
     catalogAccordion();
     catalogSorting();
+    orderForm();
 });
 
 var catalogCheckBox = function() {
@@ -27,4 +28,20 @@ var catalogSorting = function() {
         $(this).find('.accordionIcon').toggleClass('accordionIcon--active');
         $(this).next().toggleClass('catalog__sortingList--active');
     });
+};
+
+var orderForm = function() {
+    var phone = $('.orders__form').find('input[name="phone"]');
+    phone.mask("+7(999) 999-99-99");
+
+    var form = $('.orders__form');
+    var titleError = $('.orders__formErrorTitle');
+    var visionTitleError = titleError.css('display');
+
+    if (visionTitleError === 'block') {
+        form.css('margin-top', '55px');
+    }
+
+    var inputRequire = $('.orders__formItem--require > input');
+
 };
