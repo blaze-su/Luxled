0d9c0c9b0cb089c1bb499cc3a345e7c57e1ed1b0
isTerra-maksYgso <maks.kolendyuk@gmail.com>
2017-09-06 21:43:13 +0300

    правки в каталоге (добавленно слайдер в сортировку предметов)

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -2,5 +2,7 @@
 
 module.exports = [
     './bower_components/jquery/dist/jquery.min.js',
-    './bower_components/jquery.maskedinput/dist/jquery.maskedinput.min.js'
+    './bower_components/jquery.maskedinput/dist/jquery.maskedinput.min.js',
+    './bower_components/swiper/dist/js/swiper.min.js',
+    './source/js/libs/jquery-ui.min.js'
 ];
